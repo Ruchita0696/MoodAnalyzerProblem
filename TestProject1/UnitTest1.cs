1ca92c5f74076fc0ce6c3d1f1dc1cda59770c340
ruchi06 <ruchi06@gmail.com>
2023-01-25 23:37:16 +0530

    Retursad

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using MoodAnalyzerProblem;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Given_HappyOrSad_Return_HappyOrSad()
+        {
+            MoodAnalyzer mood = new MoodAnalyzer();
+            string result = mood.AnalyzeMood("Happy");
+
+            Assert.AreEqual("Happy".ToUpper(), result);
+        }
+        [TestMethod]
+        public void Given_Sad_Return_Sad()
+        {
+            MoodAnalyzer mood = new MoodAnalyzer();
+            string result = mood.AnalyzeMood("I am in Sad Mood");
+
+            Assert.AreEqual("Sad".ToUpper(), result);
+        }
+    }
+}
