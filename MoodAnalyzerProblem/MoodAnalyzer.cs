4e2d5c5ea7859d73f39b2bb647185754aba3357b
ruchi06 <ruchi06@gmail.com>
2023-01-25 18:21:06 +0530

    Happy OR Sad

---
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        public string AnalyzeMood(string message)
+        {
+            bool result1 = message.Contains("Happy", StringComparison.OrdinalIgnoreCase);
+
+            if (result1)
+            {
+                return "HAPPY";
+            }
+            bool result2 = message.Contains("Sad", StringComparison.OrdinalIgnoreCase);
+             if (result2)
+            {
+                return "SAD";
+
+            }
+            return null;
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to check Mood");
+            string mood = Console.ReadLine();
+            MoodAnalyzer analyzer = new MoodAnalyzer();
+            analyzer.AnalyzeMood(mood);
+        }
+    }
+}
